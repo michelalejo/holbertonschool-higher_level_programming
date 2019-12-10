ea0f5baefa1bf201e5dc8d3c8d7f67a2efddb266
michelalejo <michelmolina65@gmail.com>
2019-12-10 19:24:36 +0000

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for de in range(100):
     if de != 99:
-        print("{:d}, ".format(de), end="")
+        print("{0:02d}, ".format(de), end="")
     elif de == 99:
-        print("{:d}".format(de), end="\n")
+        print("{0:02d}".format(de), end="\n")
