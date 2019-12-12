c91a163126fca5422c6039e71c5b7424dbb1840f
michelalejo <michelmolina65@gmail.com>
2019-12-12 17:21:19 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    import hidden_4
+    for h in dir(hidden_4):
+        if not h.startwith("__"):
+            print("{}".format(h))
