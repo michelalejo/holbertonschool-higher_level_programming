9568c34eb32799f68181c62ebdfaadda267276df
michelalejo <michelmolina65@gmail.com>
2019-12-12 17:24:10 +0000

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for h in dir(hidden_4):
-        if not h.startwith("__"):
+        if not h.startswith("__"):
             print("{}".format(h))
