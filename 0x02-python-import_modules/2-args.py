00be69d421b61cf18cccedd18727c52d33dd4770
michelalejo <michelmolina65@gmail.com>
2019-12-12 17:07:08 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 1:
+    if len(sys.argv) - 1 > 0:
         print("{:d} arguments:".format(len(sys.argv) - 1))
         for av in range(1, len(sys.argv)):
             print("{:d}: {}".format(av, (sys.argv[av])))
