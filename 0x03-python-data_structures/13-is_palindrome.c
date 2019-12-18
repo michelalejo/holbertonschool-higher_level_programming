335b84107ccc527886cb792b81641d784d8c2ef3
michelalejo <michelmolina65@gmail.com>
2019-12-18 04:52:48 +0000

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,26 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * is_palindrome - Funtion that checks if a singly linked list is a palindrome.
- * @head: Head.
- * Return: 1 or 0.
+ * is_palindrome - test if a LL is also a palindrome
+ * @head: begining of list
+ *
+ * Return: 1 for yes, 0 for no or fail
  */
 int is_palindrome(listint_t **head)
 {
-	int i = 0, j = 0, buf[1024];
-	listint_t *pal = *head;
+	listint_t *seek;
+	int i = 0, j = 0;
+	int arr[4096];
+
 
 	if (!head)
 		return (0);
-
+	seek = *head;
 	if (!*head || (*head)->next == NULL)
 		return (1);
 
-	for (i = 0; pal; pal = pal->next, i++)
-		buf[i] = pal->n;
-
-	for (j = 0, i--; i > j; i--, j++)
+	for (; seek; seek = seek->next, i++)
+		arr[i] = seek->n;
+	for (i--; i > j; i--, j++)
 	{
-		if (buf[j] == buf[i])
+		if (arr[j] == arr[i])
 			;
 		else
 			return (0);
