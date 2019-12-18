143d5350df5b27794cbc23c4ddce2c60b0fcc2c3
michelalejo <michelmolina65@gmail.com>
2019-12-18 04:44:31 +0000

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * is_palindrome - test if a LL is also a palindrome
+ * @head: begining of list
+ * Return: 1 for yes, 0 for no or fail
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *pal;
+	int i = 0, j = 0;
+	int *arr;
+
+
+	if (!head)
+		return (0);
+	pal = *head;
+	if (!*head || (*head)->next == NULL)
+		return (1);
+
+	for (; pal; pal = pal->next, i++)
+		buf[i] = pal->n;
+	for (i--; i > j; i--, j++)
+	{
+		if (buf[j] == buf[i])
+			;
+		else
+			return (0);
+	}
+	return (1);
+}
