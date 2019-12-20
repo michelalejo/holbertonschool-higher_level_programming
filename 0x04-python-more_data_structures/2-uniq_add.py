ebb480b74445eafef14e9909a32e976af2eb3a98
michelalejo <michelmolina65@gmail.com>
2019-12-20 00:27:02 +0000

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-my_list.sort()
-cp = my_list.copy()
-k = 0
-for i in range(max(cp[1:])):
+    my_list.sort()
+    cp = my_list.copy()
+    k = 0
+    for i in range(max(cp[1:])):
         j = i - 1
         if cp[i-1] == cp[j-1]:
-                del cp[i-1]
-for j in range(len(cp)):
+            del cp[i-1]
+    for j in range(len(cp)):
         k = k + cp[j]
-return(k)
+    return(k)
