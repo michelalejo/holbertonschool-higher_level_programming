6f2d2ae34463222c0aad555d79c01def6c0473f6
michelalejo <michelmolina65@gmail.com>
2020-01-13 18:28:15 +0000

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,16 @@
+#!/usr/bin/python3
+"""
+Programt to add to number
+(a) - (b) must be and integer or float type
+return the sum of a + b
+"""
 def add_integer(a, b=98):
+    """
+    Programt to add to number
+    (a) must be and integer or float type
+    (b) must be and integer or float type
+    return the sum of a + b
+    """
     if type(a) is float or type(a) is int:
         a = int(a)
     else:
