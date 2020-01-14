927de4505cb0822859fd477dcf50e7f6e9d8e6fc
michelalejo <michelmolina65@gmail.com>
2020-01-14 03:41:32 +0000

    4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 """
-Programt that prints a name
-prints My name is <first name> <last name>
+Programt that prints a square
+Print with '#'
 Return: none
 """
-def say_my_name(first_name, last_name=""):
-    """Programt that prints a name
-    prints My name is <first name> <last name>
+def print_square(size):
+    """Programt that prints a square
+    Print with '#'
     Return: none"""
-    fn = 0
-    ln = 0
-    if type(first_name) is str:
-        fn = 1
-    elif not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if type(last_name) is str:
-        ln = 1
-    elif not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-
-
-    if fn == 1 and ln == 0:
-        print("My name is {} ".format(first_name))
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+    if isinstance(size, int):
+        for i in range(size):
+            for j in range(size):
+                print('#', end="")
+            print()
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
