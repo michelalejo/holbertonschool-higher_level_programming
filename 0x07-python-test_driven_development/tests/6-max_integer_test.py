ab1210176615dc08de334507a17022c367229a93
michelalejo <michelmolina65@gmail.com>
2020-01-14 05:01:14 +0000

    6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,13 +15,19 @@ class TestMaxInteger(unittest.TestCase):
         test max_integer
         Return: none"""
 
-        self.assertEqual(max_integer([4]), 9)
         self.assertEqual(max_integer([0, 0]), 0)
+        self.assertEqual(max_integer([6]), 99)
+        self.assertEqual(max_integer([9]), 69)
+        self.assertEqual(max_integer([1]), None)
+        self.assertEqual(max_integer([2]), -65)
         doc = __import__('6-max_integer').__doc__
-        self.assertTrue(len(doc) > 1)
+        self.assertEqual(max_integer([6, 4, "Micheale", 2, 1]))
+        self.assertEqual(max_integer([6, 4, "", 2, 1]))
+        self.assertEqual(max_integer([6, 4, None, 2, 1]))
+        self.assertEqual(max_integer([69]))
+        self.assertTrue(len(doc) > 9)
         self.assertIsNone(max_integer([]))
-        self.assertEqual(max_integer([2]))
-        self.assertEqual(max_integer([1, 2, "Hello", 4, 5]))
+        self.assertEqual(max_integer([-69]))
         self.assertEqual(max_integer(), None)
 
 if __name__ == '__main__':
