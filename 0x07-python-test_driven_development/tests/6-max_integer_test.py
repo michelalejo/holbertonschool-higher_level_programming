68b061c33389ba75c680b1bb0297db8ea93b6582
michelalejo <michelmolina65@gmail.com>
2020-01-14 05:09:38 +0000

    6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -15,20 +15,14 @@ class TestMaxInteger(unittest.TestCase):
         test max_integer
         Return: none"""
 
-        self.assertEqual(max_integer([0, 0]), 0)
-        self.assertEqual(max_integer([6]), 99)
-        self.assertEqual(max_integer([9]), 69)
-        self.assertEqual(max_integer([1]), None)
-        self.assertEqual(max_integer([2]), -65)
-        doc = __import__('6-max_integer').__doc__
-        self.assertEqual(max_integer([6, 4, "Micheale", 2, 1]))
-        self.assertEqual(max_integer([6, 4, "", 2, 1]))
-        self.assertEqual(max_integer([6, 4, None, 2, 1]))
-        self.assertEqual(max_integer([69]))
-        self.assertTrue(len(doc) > 9)
-        self.assertIsNone(max_integer([]))
-        self.assertEqual(max_integer([-69]))
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 4, 3, 2]), 4)
         self.assertEqual(max_integer(), None)
+        self.assertEqual(max_integer([2, 3.4]), 3.4)
+        self.assertEqual(max_integer([4]), 4)
+        self.assertEqual(max_integer(["hola", "amigo"]), "hola")
+        self.assertRaises(TypeError, max_integer, [1, "hola"])
+        self.assertRaises(TypeError, max_integer, None)
 
 if __name__ == '__main__':
     unittest.main()
