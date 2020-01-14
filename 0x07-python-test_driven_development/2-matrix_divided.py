ca140ccee9d6cbee45f8195e66f9718fad08c172
michelalejo <michelmolina65@gmail.com>
2020-01-14 03:43:53 +0000

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,24 +4,27 @@ Programt to div a matrix
 the elements should be a matrix
 return the div of the elementes of matrix
 """
+
+
 def matrix_divided(matrix, div):
     """Programt to div a matrix
     the elements should be a matrix
     return the div of the elementes of matrix"""
+
     m = []
     if len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists) "
-                       "of integers/floats")
+                        "of integers/floats")
     for i in matrix:
         if len(i) == 0:
             raise TypeError("matrix must be a matrix (list of lists) "
-                       "of integers/floats")
+                            "of integers/floats")
         else:
             m.append(len(i))
         for x in i:
             if type(x) is not float and type(x) is not int:
                 raise TypeError("matrix must be a matrix (list of lists) "
-                       "of integers/floats")
+                                "of integers/floats")
     if len(set(m)) != 1:
         raise TypeError('Each row of the matrix must have the same size')
     if type(div) is float or type(div) is int:
