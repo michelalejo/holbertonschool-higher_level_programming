fe5e083ad6d8b0750870012332b5377ba0597b6a
michelalejo <michelmolina65@gmail.com>
2020-01-14 05:24:09 +0000

    6-max_integer.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,8 +13,8 @@ class TestMaxInteger(unittest.TestCase):
     te_m = "'>' not supported between instances of 'str' and 'int'"
 
     def test_std_behavior(self):
-        n = [1, 2, 3]
-        self.assertEqual(max_integer(n), 3)
+        a = [1, 2, 3]
+        self.assertEqual(max_integer(a), 3)
 
     def test_negatives(self):
         self.assertEqual(max_integer([-3, 5, -2]), 5)
@@ -22,10 +22,17 @@ class TestMaxInteger(unittest.TestCase):
     def test_floats(self):
         self.assertEqual(max_integer([2.94, 1, 8.493]), 8.493)
 
+    def test_multi_lists(self):
+        a = [1, 2, 3]
+        b = [1, 9, 3]
+        c = [4, 2, 1]
+        d = [a, b, c]
+        self.assertEqual(max_integer(d), [4, 2, 1])
+
     def test_tuples(self):
         self.assertEqual(max_integer((8, 3, 12)), 12)
 
     def test_wrong_type(self):
-        n = [1, 2, 'd', 3]
+        a = [1, 2, 'd', 3]
         with self.assertRaises(TypeError, msg=self.te_m):
-            max_integer(n)
+            max_integer(a)
