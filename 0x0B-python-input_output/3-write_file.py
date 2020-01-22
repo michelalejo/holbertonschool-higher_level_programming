aa7067103157f69775de3e6706f3b39e45075270
michelalejo <michelmolina65@gmail.com>
2020-01-22 04:05:50 +0000

    3-write_file.py

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
-    j = 0
-    with open(filename, encoding='utf-8') as s:
-        for i in s:
-            j += 1
-            if nb_lines <= 0 or j <= nb_lines:
-                print(i, end="")
+    with open(filename, 'w') as s:
+        return s.write(text)
