8bb007be8d6154a5c74935c058e85814077f970e
michelalejo <michelmolina65@gmail.com>
2020-01-22 04:54:49 +0000

    12-student.py

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -5,9 +5,9 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
-        if isinstance(attrs, list):
-            if all(isinstance(i, str) for i in attrs):
+        if all(isinstance(i, str) for i in attrs):
+            if isinstance(attrs, list):
                 return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
