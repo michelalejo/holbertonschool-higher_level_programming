f8cb26402394e93a650f2696b3da91a7f870c7d8
michelalejo <michelmolina65@gmail.com>
2020-01-22 04:35:58 +0000

    9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""script that adds all arguments to a Python list, and then saves to a file"""
 save = __import__('7-save_to_json_file').save_to_json_file
 load = __import__('8-load_from_json_file').load_from_json_file
 
