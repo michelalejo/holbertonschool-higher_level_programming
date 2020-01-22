ca41c6b5cf3877d22496f2802fdc7db5f6d02b96
michelalejo <michelmolina65@gmail.com>
2020-01-21 19:10:49 -0500

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as s_t_r:
-        s = s_t_r.read()
-    print(st, end="")
+    with open(filename) as star:
+        info = star.read()
+    print(info, end="")
