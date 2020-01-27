20bfe56ca9afe0bb1286fafbd1533c887acb0c4c
michelalejo <michelmolina65@gmail.com>
2020-01-27 19:48:42 +0000

    rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"Rectangle class"
+from models.base import Base
+
+
+class Rectangle(Base):
+    "Rectangle class"
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        "Rectangle class"
+        super().__init__(id)
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if type(x) is not int:
+            raise TypeError('x must be an integer')
+        if type(y) is not int:
+            raise TypeError('y must be an integer')
+        if width <= 0:
+            raise ValueError('width must be > 0')
+        if height <= 0:
+            raise ValueError('height must be > 0')
+        if x < 0:
+            raise ValueError('x must be >= 0')
+        if y < 0:
+            raise ValueError('y must be >= 0')
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width (self):
+        "Rectangle, width getter"
+        return (self.__width)
+
+    @width.setter
+    def width (self, value):
+        "Rectangle, width setter"
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width musst be > 0')
+        self.__width = value
+
+    @property
+    def height (self):
+        "Rectangle, height getter"
+        return (self.__height)
+
+    @height.setter
+    def height (self, value):
+        "Rectangle, width setter"
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height musst be > 0')
+        self.__height = value
+
+    @property
+    def x(self):
+        "Rectangle, height getter"
+        return (self.__x)
+
+    @x.setter
+    def x (self, value):
+        "Rectangle, width setter"
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value <= 0:
+            raise ValueError('x musst be > 0')
+        self.__x = value
+
