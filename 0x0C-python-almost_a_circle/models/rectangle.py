27c80f767155cf1352727b46200331331bdf2272
michelalejo <michelmolina65@gmail.com>
2020-01-27 21:44:08 +0000

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,7 +78,11 @@ class Rectangle(Base):
 
     def display(self):
         "Rectangle Display"
+        for f in range(self.__y):
+            print("")
         for i in range(self.__height):
+            for h in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
