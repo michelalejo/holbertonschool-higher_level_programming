4f69109fab3afedb14fd9bf94f97d1b533dda74e
michelalejo <michelmolina65@gmail.com>
2020-01-28 03:15:53 +0000

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,17 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Base Staticmetohd"""
+        "Base Staticmetohd"
         if list_dictionaries is None:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        "Base Classmetohd"
+        dic = []
+        if list_objs:
+            for i in list_objs:
+                dic.append(cls.to_dictionary(i))
+        with open(cls.__name__ + ".json", "w") as file:
+            file.write(cls.to_json_string(dic))
