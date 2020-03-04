2199e652803411b36ecbbb926c35dd4b37937bb5
michelalejo <michelmolina65@gmail.com>
2020-03-04 04:30:50 +0000

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,5 @@
 -- MySQL Script.
-SELECT `score`, `name` FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
