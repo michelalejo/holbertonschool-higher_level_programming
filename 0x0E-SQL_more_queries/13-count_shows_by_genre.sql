7cdd63626a217e5ac277f1d84acb7ce21efb7827
michelalejo <michelmolina65@gmail.com>
2020-03-04 04:45:02 +0000

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- MySQL Script.
-SELECT `score`, `name` FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT g.name AS genre, COUNT(g.name) AS number_of_shows FROM tv_shows s INNER JOIN tv_show_genres sg ON s.id = sg.show_id INNER JOIN tv_genres g ON g.id = sg.genre_id GROUP BY g.name ORDER BY number_of_shows DESC;
