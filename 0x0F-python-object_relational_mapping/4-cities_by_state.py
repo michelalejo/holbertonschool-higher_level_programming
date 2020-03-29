a1beb537ab2590b828d357043fc14c45c33d9f6a
michelalejo <michelmolina65@gmail.com>
2020-03-29 21:23:48 +0000

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that lists all states from a database"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+        dic = {
+                'host': "localhost",
+                'port': 3306,
+                'user': argv[1],
+                'passwd': argv[2],
+                'db': argv[3]
+        }
+        command = """select * from cities order by id"""
+
+        db = MySQLdb.connect(**dic)
+        cursor = db.cursor()
+        cursor.execute(command)
+
+        results = cursor.fetchall()
+        for dates in results:
+                print(dates)
+
+        cursor.close()
+        db.close()
