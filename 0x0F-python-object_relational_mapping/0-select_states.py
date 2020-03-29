170ed8a998f6d47c6571c1fa02bb14c8538cf239
michelalejo <michelmolina65@gmail.com>
2020-03-29 07:45:20 +0000

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that lists all states from a database"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        exit(-1)
+dic = {
+    'host': "localhost",
+    'port': 3306,
+    'user': argv[1],
+    'passwd': argv[2],
+    'db': argv[3]
+}
+command = "SELECT * FROM states ORDER BY states.id"
+
+db = MySQLdb.connect(**dic)
+cursor = db.cursor()
+cursor.execute(command)
+
+results = cursor.fetchall()
+for dates in results:
+    print(dates)
