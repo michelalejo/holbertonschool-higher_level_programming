8f9fd0039ae7753d8cedb002f478f82943e0fb67
michelalejo <michelmolina65@gmail.com>
2020-03-29 17:17:00 +0000

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that lists all states from a database"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    dic = {
+        'host': "localhost",
+        'port': 3306,
+        'user': argv[1],
+        'passwd': argv[2],
+        'db': argv[3]
+    }
+    command = "select * from states where name = %s order by id"
+
+    db = MySQLdb.connect(**dic)
+    cursor = db.cursor()
+    cursor.execute(command, (argv[4],))
+
+    results = cursor.fetchall()
+    for dates in results:
+        print(dates)
+
+    cursor.close()
+    db.close()
