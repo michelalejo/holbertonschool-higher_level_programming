6847767367936886db314392a1f922e54e81be91
michelalejo <michelmolina65@gmail.com>
2020-03-30 04:42:42 +0000

    model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that lists all states from a database"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False,
+                autoincrement="auto")
+    name = Column(String(128),
+                nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
