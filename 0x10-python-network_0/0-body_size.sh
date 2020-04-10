bfc1dcde3aaf7e983a10dcf3b1fe9cb3c0a0bf50
michelalejo <michelmolina65@gmail.com>
2020-04-10 21:05:20 +0000

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that displays the size of the body of the response.
 
-curl -sI "$1" -X GET | grep "Content-Length" | cut -d ' ' -f2
+curl -sI "$1" | grep "Content-Length" | cut -d ':' -f2 | tr -d ' '
