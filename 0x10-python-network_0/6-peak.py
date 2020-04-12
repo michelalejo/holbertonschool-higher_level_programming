cc4fbaafe3c1f05a14fc58dcf58f1949710e4b2d
michelalejo <michelmolina65@gmail.com>
2020-04-12 07:26:25 +0000

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Find the Peak"""
 def find_peak(list_of_integers):
+    if list_of_integers == []:
+        return
     max = 0
     for n in list_of_integers:
         if n > max:
             max = n
-    if max == 0:
-        return (None)
     return (max)
