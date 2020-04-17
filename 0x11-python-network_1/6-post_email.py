44b31caf60d8eeca2bfce8b201b9933d9e971ca8
michelalejo <michelmolina65@gmail.com>
2020-04-17 03:20:04 +0000

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Script that takes URL and a email, sends POST reques tto the URL"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.post(argv[1], data={'email': argv[2]})
+    print(res.text)
