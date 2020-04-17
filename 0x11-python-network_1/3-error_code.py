9edf6ff53655cfa846feca7095e0554742b21304
michelalejo <michelmolina65@gmail.com>
2020-04-17 03:17:33 +0000

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Script that takes URL and a email, sends POST reques tto the URL"""
+
+from urllib import request, parse, error
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print("Error code:", err.code)
