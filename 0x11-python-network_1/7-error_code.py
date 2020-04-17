75a4297350c5d5569d6fb91a237421032d198ffc
michelalejo <michelmolina65@gmail.com>
2020-04-17 03:25:10 +0000

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Script that takes URL and a email, sends POST reques tto the URL"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    res = requests.get(argv[1])
+    if res.status_code >= 400:
+        print("Error code:", res.status_code)
+    else:
+        print(res.text)
