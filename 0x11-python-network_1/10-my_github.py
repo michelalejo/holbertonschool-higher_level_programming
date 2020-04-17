f5633add72725d38c8cbb0ec3cb2e4ed2177c44a
michelalejo <michelmolina65@gmail.com>
2020-04-17 03:41:32 +0000

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""""Script that takes your Github credentials"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.exit()
+    q = (sys.argv[1], sys.argv[2])
+    html = requests.get('https://api.github.com/user', auth=q).json()
+    if 'id' in html:
+        try:
+            print(str(html['id']))
+        except ValueError:
+            print("Not a valid JSON")
+    else:
+        print("None")
