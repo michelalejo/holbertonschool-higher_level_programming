29b80fad731f0be1ced0f7a77616eb053282a7a9
michelalejo <michelmolina65@gmail.com>
2020-04-17 03:28:08 +0000

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Script that takes URL and a email, sends POST reques tto the URL"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        q = ""
+    else:
+        q = argv[1]
+
+    res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+
+    try:
+        data = res.json()
+        if data:
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
