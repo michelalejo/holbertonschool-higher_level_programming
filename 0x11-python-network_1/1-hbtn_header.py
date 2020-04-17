0323b0971b20224cb61bcfd334dea83be0620ed5
michelalejo <michelmolina65@gmail.com>
2020-04-17 00:36:24 +0000

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Script that takes in a URL, sends arequest to the URL, displays the value"""
+
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as res:
+        print(res.headers.get('X-Request-Id'))
