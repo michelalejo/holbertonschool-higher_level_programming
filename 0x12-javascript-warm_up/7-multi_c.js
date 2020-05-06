6fb815828ebc8f810d74b47a54c8403db4bb29a8
michelalejo <michelmolina65@gmail.com>
2020-05-06 03:27:36 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const string = ['C is fun'];
 let i = 0;
 const n = parseInt(process.argv[2], 10);
-while (i < n && !(isNaN(n))) {
-  console.log(string);
-  i++;
-}
 if (isNaN(n)) {
   console.log('Missing number of occurrences');
+} else {
+  while (i < n) {
+    console.log('C is fun');
+    i++;
+  }
 }
