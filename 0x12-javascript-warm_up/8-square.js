d7037c5f52ab71af164a4669ef1a74bd972481a1
michelalejo <michelmolina65@gmail.com>
2020-05-06 04:08:35 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const n = parseInt(process.argv[2], 10);
+let square = '';
+let i = n;
+let j = n;
+if (isNaN(n)) {
+  console.log('Missing number of occurrences');
+} else {
+  while (i > 0) {
+    while (j > 0) {
+      square += 'X';
+      j--;
+    }
+    console.log(square);
+    i--;
+  }
+}
