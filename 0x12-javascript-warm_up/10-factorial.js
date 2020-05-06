2d7b743921077e51690ca05ac2687342df897332
michelalejo <michelmolina65@gmail.com>
2020-05-06 04:34:34 +0000

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const n = parseInt(process.argv[2]);
+function factorial (n) {
+  if (process.argv.length <= 2 || n === 0) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
+}
+console.log(factorial(n));
