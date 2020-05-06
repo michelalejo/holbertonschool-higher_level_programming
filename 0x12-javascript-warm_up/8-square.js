d1ac66048f5f344788e920b172d953ea01358d2d
michelalejo <michelmolina65@gmail.com>
2020-05-06 04:11:19 +0000

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ let square = '';
 let i = n;
 let j = n;
 if (isNaN(n)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   while (i > 0) {
     while (j > 0) {
