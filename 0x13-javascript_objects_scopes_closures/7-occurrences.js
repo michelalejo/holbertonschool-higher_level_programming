dbeb3ead13decd453f2dc469cd05e3f523d53a10
michelalejo <michelmolina65@gmail.com>
2020-05-12 04:24:27 +0000

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let n = 0;
+  let i = 0;
+  for (i = 0; i <= list.length; i++) {
+    if (list[i] === searchElement) {
+      n = n + 1;
+    }
+  }
+  return (n);
+};
