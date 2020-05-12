e6057a899aee8ec8b8e9bd65ffd719fc28f9526c
michelalejo <michelmolina65@gmail.com>
2020-05-12 02:41:01 +0000

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let square = '';
+    let i = this.height;
+    let j = this.width;
+    while (i > 0) {
+      while (j > 0) {
+        square += 'X';
+        j--;
+      }
+      console.log(square);
+      i--;
+    }
+  }
+}
+module.exports = Rectangle;
