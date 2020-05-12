c4527d496a7a81e6f1a25db1ad196acb3fb48466
michelalejo <michelmolina65@gmail.com>
2020-05-12 04:31:51 +0000

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  const tmp = [];
+  let i = 0;
+  let size = list.length;
+  for (i = 0; i < list.length; i++, size--) {
+    tmp[i] = list[size - 1];
+  }
+  return (tmp);
+};
