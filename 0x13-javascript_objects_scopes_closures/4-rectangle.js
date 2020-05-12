082d199dfa99b3098cfa8e4c7e922d2656d660fc
michelalejo <michelmolina65@gmail.com>
2020-05-12 02:50:04 +0000

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,36 @@
 #!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let square = '';
+    let i = this.height;
+    let j = this.width;
+    while (i > 0) {
+      while (j > 0) {
+        square += 'X';
+        j--;
+      }
+      console.log(square);
+      i--;
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
