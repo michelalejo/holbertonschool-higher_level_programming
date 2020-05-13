87600742f88b7189a6a871576a81c1c99b743777
michelalejo <michelmolina65@gmail.com>
2020-05-13 04:57:45 +0000

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const av = process.argv.slice(2);
+const url = av[0];
+const path = av[1];
+request.get(url, (err, resp, body) => {
+  if (err) throw err;
+  fs.writeFile(path, body, 'utf8', (error) => {
+    if (error) throw error;
+  });
+});
