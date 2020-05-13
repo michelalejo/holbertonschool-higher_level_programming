75002f29381002562dded18af9abf925eb53baf8
michelalejo <michelmolina65@gmail.com>
2020-05-13 04:53:03 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const request = require('request');
+let n = 0;
+request.get(process.argv[2], (error, resp, body) => {
+  if (error) throw error;
+  for (const film of JSON.parse(body).results) {
+    for (const line of film.characters) {
+      if (line.endsWith('18/')) {
+        n++;
+      }
+    }
+  }
+  console.log(n);
+});
