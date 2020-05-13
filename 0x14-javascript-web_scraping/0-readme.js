434e8f6b5e598797738bfe7c753fed358bb77d8c
michelalejo <michelmolina65@gmail.com>
2020-05-13 04:38:28 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+const source = process.argv[2];
+
+fs.readFile(source, 'utf8', (err, source) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(source);
+});
