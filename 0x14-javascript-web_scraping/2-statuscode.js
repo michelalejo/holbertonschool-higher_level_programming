8fd85e4c2fdae369ae2a6354a20846b61ff99da5
michelalejo <michelmolina65@gmail.com>
2020-05-13 04:45:39 +0000

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error) throw error;
+  console.log('code: ' + response.statusCode);
+});
