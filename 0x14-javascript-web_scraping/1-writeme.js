18849cfcdc5dba280bff5c14f67f8441af6a3395
michelalejo <michelmolina65@gmail.com>
2020-05-13 04:40:53 +0000

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const fs = require('fs');
+const av = process.argv.slice(2);
+const path = av[0];
+const data = av[1];
+fs.writeFile(path, data, 'utf8', (err) => {
+  if (err) { console.log(err); }
+});
