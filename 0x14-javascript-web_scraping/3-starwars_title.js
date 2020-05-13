ef02982b87c41461d6b6355f822b7fec6abe15f1
michelalejo <michelmolina65@gmail.com>
2020-05-13 04:48:55 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(url, (error, response, body) => {
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
+});
