c3fcb653443ada14862d20f4f4864edf3df3d32e
michelalejo <michelmolina65@gmail.com>
2020-05-13 04:59:23 +0000

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, { json: true }, (err, res, body) => {
+  if (err) { console.log(err); } else {
+    const result = {};
+    for (const todo of body) {
+      if (todo.completed) {
+        if (result[todo.userId]) {
+          result[todo.userId] += 1;
+        } else {
+          result[todo.userId] = 1;
+        }
+      }
+    }
+    console.log(result);
+  }
+});
