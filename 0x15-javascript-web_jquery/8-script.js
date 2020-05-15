b9359a8d2548967476890f9abbc6d2f5da51f9c8
Michel Molina <michelmolina65@gmail.com>
2020-05-14 23:14:33 -0500

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $('.result').html(data);
+  let i = 0;
+  for (i = 0; i < data.count; i++) {
+    $('#list_movies').add('<li>' + data.results[i].title + '</li>').appendTo('#list_movies');
+  }
+});
